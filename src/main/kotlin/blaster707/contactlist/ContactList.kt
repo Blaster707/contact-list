05cb364bc4c7dab3f7e52d26f8ea58528fb60453
Lucas Ferguson <blaster707dc@gmail.com>
2022-12-01 19:20:25 -0600

    Removed erroneous code

--- src/main/kotlin/blaster707/contactlist/ContactList.kt
+++ src/main/kotlin/blaster707/contactlist/ContactList.kt
@@ -5,15 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ContactList(val contacts: MutableList<ContactEntry>) {
 
-    companion object ContactListFunctions {
-        /*fun contactListIDSet(contactList: ContactList): Set<String>{
-            var idSet: Set<String> = setOf()
-            for (contactEntry in contactList.array) {
-                idSet = idSet.plus(contactEntry.id)
-            }
-            return idSet
-        }*/
-    }
     fun contactListAll(): String{
         var output: String = ""
         for (contactEntry in contacts) {
